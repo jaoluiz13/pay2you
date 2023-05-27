4de6abaf1bea5db9e4cd8c74c02ecfb8875eb6eb
jaoluiz13 <luizcostajoao12@gmail.com>
2023-05-27 20:18:27 -0300

    feat: starting test environment

--- src/test/java/com/api/pay2you/Pay2youApplicationTests.java
+++ src/test/java/com/api/pay2you/Pay2youApplicationTests.java
@@ -2,8 +2,10 @@ package com.api.pay2you;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class Pay2youApplicationTests {
 
 	@Test
