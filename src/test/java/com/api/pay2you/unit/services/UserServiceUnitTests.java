62abe1088ce08f9b0c18c9068481dbf78cfa9533
jaoluiz13 <luizcostajoao12@gmail.com>
2023-05-28 16:04:05 -0300

    fix: fixing project

--- src/test/java/com/api/pay2you/unit/services/UserServiceUnitTests.java
+++ src/test/java/com/api/pay2you/unit/services/UserServiceUnitTests.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
